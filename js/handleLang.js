88f3d6fdc16f970c677adc160947833642f7a20a
Milan <48488190+MilanJulinac42@users.noreply.github.com>
2021-08-07 00:02:56 +0200

    language change

---
+++ js/handleLang.js
@@ -0,0 +1,48 @@
+import lang from "../language/lang.js";
+
+const data = lang.data;
+let defaulL = "srb";
+
+const typeL = window.location.href.split("?")[1];
+defaulL = typeL.split("=")[1];
+
+const langWrap = document.querySelector(".lang");
+const language = document.querySelectorAll(".language");
+const links = document.querySelectorAll(".a-link");
+
+if (defaulL == "srb") {
+  langWrap.querySelector(".active-lang").classList.remove("active-lang");
+  language[0].classList.add("active-lang");
+} else if (defaulL == "eng") {
+  langWrap.querySelector(".active-lang").classList.remove("active-lang");
+  language[1].classList.add("active-lang");
+}
+
+language.forEach((el) => {
+  el.addEventListener("click", () => {
+    langWrap.querySelector(".active-lang").classList.remove("active-lang");
+    el.classList.add("active-lang");
+
+    const attr = el.getAttribute("language");
+    setLan(attr);
+  });
+});
+
+function setLan(attr) {
+  let counter = 0;
+  for (let key in data[attr].navigation) {
+    let value = data[attr].navigation[key];
+    links[counter].textContent = value;
+    links[counter].setAttribute(
+      "href",
+      setUrl(links[counter].getAttribute("href")) + `?language=${attr}`
+    );
+    counter++;
+  }
+}
+
+function setUrl(url) {
+  return url.split("?")[0];
+}
+
+setLan(defaulL);
---
+++ language/lang.js
@@ -0,0 +1,22 @@
+export default {
+  data: {
+    eng: {
+      navigation: {
+        mainPage: "Main",
+        graphicDesign: "Graphic design",
+        webDesign: "Web design",
+        videoAnimation: "Video & animation",
+        contact: "Contact",
+      },
+    },
+    srb: {
+      navigation: {
+        mainPage: "Početna",
+        graphicDesign: "Grafički dizajn",
+        webDesign: "Web dizajn",
+        videoAnimation: "Video i animacija",
+        contact: "Kontakt",
+      },
+    },
+  },
+};

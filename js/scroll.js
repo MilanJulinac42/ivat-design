bcee838926aaf3fa5c6c01232893e632079a5470
Milan <48488190+MilanJulinac42@users.noreply.github.com>
2021-08-08 15:22:02 +0200

    portfolio

--- js/scroll.js
+++ js/scroll.js
@@ -18,7 +18,7 @@ window.addEventListener("scroll", () => {
       "../assets/logoi/misc/insta sivi.png";
     document.getElementById("beli-fb").src = "../assets/logoi/misc/fb sivi.png";
     changeColor(llinks, "#474849");
-  } else if (window.scrollY < window.innerHeight - 200) {
+  } else {
     document.getElementById("ivat").src = "../assets/logoi/misc/beli.png";
     document.getElementById("beli-insta").src =
       "../assets/logoi/misc/insta beli.png";

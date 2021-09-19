111af9789dce9f341bd4146cc62fc439163dcffe
Milan <48488190+MilanJulinac42@users.noreply.github.com>
2021-09-19 17:24:51 +0200

    modal

---
+++ js/handlePortfolioPlay.js
@@ -0,0 +1,51 @@
+const friki = document.querySelector(".friki");
+const krug = document.querySelector(".krug");
+const pcmax = document.querySelector(".pcmax");
+const modal = document.getElementById("myModal");
+const frame = document.getElementById("modalIframe");
+
+function disableScrolling() {
+  var x = window.scrollX;
+  var y = window.scrollY;
+  window.onscroll = function () {
+    window.scrollTo(x, y);
+  };
+}
+
+function enableScrolling() {
+  window.onscroll = function () {};
+}
+
+function handleBtnClick() {
+  modal.style.display = "flex";
+  modal.style.transform = "translateY(0px)";
+  modal.scrollIntoView({
+    behavior: "auto",
+    block: "center",
+    inline: "center",
+  });
+  disableScrolling();
+}
+
+friki.addEventListener("click", () => {
+  handleBtnClick();
+  frame.src = "https://www.youtube.com/embed/A6XUVjK9W4o";
+});
+
+krug.addEventListener("click", () => {
+  handleBtnClick();
+  frame.src = "https://www.youtube.com/embed/A6XUVjK9W4o";
+});
+
+pcmax.addEventListener("click", () => {
+  handleBtnClick();
+  frame.src = "https://www.youtube.com/embed/A6XUVjK9W4o";
+});
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.transform = "translateY(-300px)";
+    modal.style.display = "none";
+  }
+  enableScrolling();
+};
